6471a8351c074839ea6ec35cf1c5c5f2f1ef9c87
karl <karl@df406022-fe11-11dd-a3ef-1f8437fb274a>
2009-02-16 08:02:04 +0000

    New proc 'assemble' that assembles multi-line log messages into single line messages when postgresql messages are recognized.

--- programs/fa_pg_log_groper/main.tcl
+++ programs/fa_pg_log_groper/main.tcl
@@ -2,12 +2,14 @@
 #
 #
 #
-#
+# $Id: main.tcl,v 1.3 2009-02-16 08:02:04 karl Exp $
 #
 
 package require Tclx
 #catch {parray foo}
 #cmdtrace on
+
+set sequence 0
 
 
 set logPattern {^(...............) ([^ ]*) ([^[]*)\[([^\]]*)]: (.*)}
@@ -16,6 +18,9 @@ set postgresPattern {\[([^-]*)-([^\]]*)] *(.*)}
 
 #Feb 16 00:29:44 zulu postgres[44626]: [21-20]check_barr_override(b.ident,b.filed_departuretime,b.blocked) as blocked,
 
+#
+# crack_line - crack a log line into an array
+#
 proc crack_line {line arrayName} {
     global logPattern postgresPattern
     upvar $arrayName array
@@ -29,16 +34,54 @@ proc crack_line {line arrayName} {
 	set array(pgFragment) [string trim $array(pgFragment)]
     }
 
-    parray array
-    puts ""
+    #parray array
+    #puts ""
 }
+
+#
+# assemble - given an array from a cracked log line, assemble multiple lines
+#  into coherent stuff
+#
+proc assemble {arrayName} {
+    global collector sequences sequence
+    upvar $arrayName array
+
+    if {![info exists array(pgMajorID)]} {
+	return
+    }
+
+    if {$array(pgMinorID) == 1} {
+	incr sequence
+    }
+
+    set id $array(pid)-$array(pgMajorID)
+    set sequences($id) $sequence
+    append collector($id) "$array(pgFragment) "
+
+    scan_for_completes
+}
+
+proc scan_for_completes {} {
+    global collector sequences sequence
+
+    foreach element [array names sequences] {
+	if {$sequences($element) + 10 < $sequence} {
+	    puts "$element,$collector($element)"
+	    puts ""
+	    unset sequences($element) collector($element)
+	}
+    }
+}
+
 
 proc run {} {
     set fp [open /var/log/postgres.log]
 
     while {[gets $fp line] >= 0} {
-	puts $line
+	unset -nocomplain array
+	#puts $line
 	crack_line $line array
+	assemble array
     }
 
     close $fp
