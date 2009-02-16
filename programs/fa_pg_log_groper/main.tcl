1a3beb60dab94215636002ea1f86c6529570878f
karl <karl@df406022-fe11-11dd-a3ef-1f8437fb274a>
2009-02-16 07:49:21 +0000

    Crack fields out of the log lines.  If they're postgres line, crack addition stuff out.

--- programs/fa_pg_log_groper/main.tcl
+++ programs/fa_pg_log_groper/main.tcl
@@ -9,14 +9,26 @@ package require Tclx
 #catch {parray foo}
 #cmdtrace on
 
+
+set logPattern {^(...............) ([^ ]*) ([^[]*)\[([^\]]*)]: (.*)}
+
+set postgresPattern {\[([^-]*)-([^\]]*)] *(.*)}
+
 #Feb 16 00:29:44 zulu postgres[44626]: [21-20]check_barr_override(b.ident,b.filed_departuretime,b.blocked) as blocked,
 
 proc crack_line {line arrayName} {
+    global logPattern postgresPattern
     upvar $arrayName array
 
-    set pattern {^(...............) ([^ ]*) ([^[]*)\[([^\]]*)]: (.*)}
-    puts [regexp $pattern $line dummy array(date) array(host) array(program) array(pid) array(body)]
-    #\[([^-]*)-([^\]]*)](.*)
+    if {![regexp $logPattern $line dummy array(date) array(host) array(program) array(pid) array(body)]} {
+	puts stderr "couldn't crack: $line"
+	return
+    }
+
+    if {[regexp $postgresPattern $array(body) dummy array(pgMajorID) array(pgMinorID) array(pgFragment)]} {
+	set array(pgFragment) [string trim $array(pgFragment)]
+    }
+
     parray array
     puts ""
 }
